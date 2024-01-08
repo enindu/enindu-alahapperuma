c3a1b699fdd92dbc14ba39300b8f9f2b417e1714
Enindu Alahapperuma <enindu@gmail.com>
2024-01-09 02:49:39 +0530

    Add rel-me links of social media accounts

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -27,6 +27,12 @@
     <link href="{{ url()->current() }}" rel="canonical">
     <link href="{{ url('rss.xml') }}" rel="alternate" title="{{ env('APP_NAME') }}" type="application/rss+xml">
     <link href="https://mastodon.social/@enindu" rel="me">
+    <link href="https://github.com/enindu" rel="me">
+    <link href="https://gitlab.com/enindu" rel="me">
+    <link href="https://www.linkedin.com/in/enindu" rel="me">
+    <link href="https://twitter.com/Enindu" rel="me">
+    <link href="https://www.reddit.com/user/Enindu" rel="me">
+    <link href="https://www.pinterest.com/enindu" rel="me">
     <link href="{{ Vite::asset('resources/images/favicon.png') }}" rel="icon" type="image/png">
     <link href="{{ Vite::asset('resources/images/apple-touch-icon.png') }}" rel="apple-touch-icon">
     @vite('resources/css/style.css')
