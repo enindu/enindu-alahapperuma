1fde2bcc09f2ccc63c24ac645fe38ba39bd6c11b
Enindu Alahapperuma <enindu@gmail.com>
2024-01-12 06:12:31 +0530

    Update footer

--- resources/views/components/includes/footer.blade.php
+++ resources/views/components/includes/footer.blade.php
@@ -1,1 +1,5 @@
-<footer class="my-3 text-center">&copy; 2021-{{ date('Y') }} <a href="{{ route('index') }}">{{ env('APP_NAME') }}</a>. Licensed under <a href="https://github.com/enindu/enindu-alahapperuma/blob/master/LICENSE.md">MIT license</a>.</footer>
+<div class="card my-3">
+    <div class="card-body">
+        <footer class="text-center">&copy; 2021-{{ date('Y') }} <a href="{{ route('index') }}">{{ env('APP_NAME') }}</a>. Licensed under <a href="https://github.com/enindu/enindu-alahapperuma/blob/master/LICENSE.md">MIT license</a>.</footer>
+    </div>
+</div>
