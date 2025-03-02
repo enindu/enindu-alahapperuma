98f27d3d3c7ea92f4a4368b88d59bb41eaa0965c
Enindu Alahapperuma <enindu@gmail.com>
2025-03-02 18:51:02 +0530

    Update header

--- resources/views/components/includes/header.blade.php
+++ resources/views/components/includes/header.blade.php
@@ -1,13 +1,13 @@
 <div class="card rounded-5 p-3">
     <div class="card-body">
-        <header>
-            <div class="d-flex justify-content-between flex-row">
-                <div class="d-flex flex-column flex-lg-row gap-lg-3 gap-0">
+        <header class="header-wrapper">
+            <div class="d-flex justify-content-between flex-row gap-3">
+                <div class="d-flex flex-row gap-3">
                     <a class="text-decoration-none {{ request()->routeIs('index') ? 'active' : '' }} text-white" href="{{ route('index') }}">Home</a>
                     <a class="text-decoration-none {{ request()->routeIs('services.*') ? 'active' : '' }} text-white" href="{{ route('services.index') }}">Services</a>
                     <a class="text-decoration-none {{ request()->routeIs('contact.*') ? 'active' : '' }} text-white" href="{{ route('contact.index') }}">Contact</a>
                 </div>
-                <div class="d-flex flex-column flex-lg-row gap-lg-3 gap-0">
+                <div class="d-flex flex-row gap-3">
                     <a class="text-decoration-none text-white" href="{{ url('sitemap.xml') }}">Sitemap</a>
                 </div>
             </div>
