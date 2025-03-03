2988040a892e6bc6946a62a62ef6c765af50497b
Enindu Alahapperuma <enindu@gmail.com>
2025-03-03 15:34:49 +0530

    Fix z index issue in preloader

--- resources/views/components/includes/preloader.blade.php
+++ resources/views/components/includes/preloader.blade.php
@@ -1,3 +1,3 @@
-<div class="preloader-wrapper d-flex align-items-center justify-content-center position-fixed h-100 w-100 left-0 top-0 z-1" id="preloader-wrapper">
+<div class="preloader-wrapper d-flex align-items-center justify-content-center position-fixed h-100 w-100 left-0 top-0 z-2" id="preloader-wrapper">
     <img alt="{{ env('APP_NAME') }} Logo" class="preloader" id="preloader" src="{{ Vite::asset('resources/images/logo.svg') }}">
 </div>
