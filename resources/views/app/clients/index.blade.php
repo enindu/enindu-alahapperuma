8cabcd31a93e9577bd491c5425561c4aa1bc4318
Enindu Alahapperuma <enindu@gmail.com>
2025-04-21 16:18:50 +0530

    Add Elegant Aquatics URL

--- resources/views/app/clients/index.blade.php
+++ resources/views/app/clients/index.blade.php
@@ -18,7 +18,7 @@
     <div class="card rounded-5 p-3">
         <div class="card-body">
             <div class="d-flex flex-column gap-3 gap-lg-0">
-                <a class="text-white text-decoration-none" href="#" target="_blank">Elegant Aquatic</a>
+                <a class="text-white text-decoration-none" href="https://elegantaquatics.co.uk" target="_blank">Elegant Aquatics</a>
                 <a class="text-white text-decoration-none" href="https://xynosync.com" target="_blank">XynoSync</a>
                 <a class="text-white text-decoration-none" href="https://crafterscollection.com" target="_blank">Crafters Collection</a>
                 <a class="text-white text-decoration-none" href="https://dubsndot.com.au" target="_blank">dubsndot</a>
