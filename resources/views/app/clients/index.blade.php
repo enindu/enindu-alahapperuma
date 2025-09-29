59279ad8875752c1c2dd2336605fbd06ab2ee16e
Enindu Alahapperuma <enindu@gmail.com>
2025-09-29 15:06:04 +0530

    Update clients page

--- resources/views/app/clients/index.blade.php
+++ resources/views/app/clients/index.blade.php
@@ -17,18 +17,149 @@
     </div>
     <div class="card rounded-5 p-3">
         <div class="card-body">
-            <div class="d-flex flex-column gap-3 gap-lg-0">
-                <a class="text-white text-decoration-none" href="https://ynjenlighten.com" target="_blank">YNJ Enlighten</a>
-                <a class="text-white text-decoration-none" href="https://banaano.com" target="_blank">Banaano</a>
-                <a class="text-white text-decoration-none" href="https://elegantaquatics.co.uk" target="_blank">Elegant Aquatics</a>
-                <a class="text-white text-decoration-none" href="#" target="_blank">Trans Asia Maldives</a>
-                <a class="text-white text-decoration-none" href="https://xynosync.com" target="_blank">XynoSync</a>
-                <a class="text-white text-decoration-none" href="https://crafterscollection.com" target="_blank">Crafters Collection</a>
-                <a class="text-white text-decoration-none" href="https://dubsndot.com.au" target="_blank">dubsndot</a>
-                <a class="text-white text-decoration-none" href="#" target="_blank">Colombo Business Association</a>
-                <a class="text-white text-decoration-none" href="#" target="_blank">TicketZone</a>
-                <a class="text-white text-decoration-none" href="https://phyxle.com" target="_blank">Phyxle</a>
-                <a class="text-white text-decoration-none" href="https://novart.lk" target="_blank">Novart</a>
+            <div class="d-flex flex-column gap-3">
+                <div class="d-flex flex-column gap-3">
+                    <h2 class="mb-0">Freelance Clients</h2>
+                    <p class="mb-0">As a full-time freelancer, I have delivered a diverse range of services to my clients, including web development, system administration, digital marketing, and content creation, helping businesses and individuals establish their online presence and achieve their goals.</p>
+                    <div class="d-flex flex-column gap-3 gap-lg-0">
+                        <a class="text-white text-decoration-none" href="https://ynjenlighten.com" target="_blank">YNJ Enlighten</a>
+                        <a class="text-white text-decoration-none" href="https://banaano.com" target="_blank">Banaano</a>
+                        <a class="text-white text-decoration-none" href="https://elegantaquatics.co.uk" target="_blank">Elegant Aquatics</a>
+                        <p class="mb-0">Trans Asia Maldives</p>
+                        <a class="text-white text-decoration-none" href="https://xynosync.com" target="_blank">XynoSync</a>
+                        <p class="mb-0">OOThoughts</p>
+                        <a class="text-white text-decoration-none" href="https://dubsndot.com.au" target="_blank">dubsndot</a>
+                        <a class="text-white text-decoration-none" href="https://crafterscollection.com" target="_blank">Crafters Collection</a>
+                        <a class="text-white text-decoration-none" href="https://phyxle.com" target="_blank">Phyxle</a>
+                        <p class="mb-0">Colombo Business Association</p>
+                        <p class="mb-0">TicketZone</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card rounded-5 p-3">
+        <div class="card-body">
+            <div class="d-flex flex-column gap-3">
+                <div class="d-flex flex-column gap-3">
+                    <h2 class="mb-0">XynoSync Clients</h2>
+                    <p class="mb-0">I co-founded XynoSync to offer a different approach to solving client problems and achieving their goals. The company was established to serve clients with a programmatic, security-focused approach rather than relying solely on marketing materials, while ensuring full transparency between the company and its clients. At XynoSync, I have had the opportunity to collaborate with numerous clients and developers, providing my expertise in web development, system administration, and consulting services.</p>
+                    <div class="d-flex flex-column gap-3 gap-lg-0">
+                        <a class="text-white text-decoration-none" href="https://ciaolankatours.com" target="_blank">Ciao Lanka Tours & Adventure</a>
+                        <p class="mb-0">Wahana Pola</p>
+                        <p class="mb-0">Devil Tours & Taxi Service</p>
+                        <a class="text-white text-decoration-none" href="https://prowear.lk" target="_blank">Pro Wear</a>
+                        <a class="text-white text-decoration-none" href="https://everfreightexpress.com" target="_blank">Everfreight Express</a>
+                        <a class="text-white text-decoration-none" href="https://pragathieducation.lk" target="_blank">Pragathi Education</a>
+                        <a class="text-white text-decoration-none" href="https://taprobanaapparel.com" target="_blank">Taprobana Apparel</a>
+                        <a class="text-white text-decoration-none" href="https://futuredreams.lk" target="_blank">Future Dreams & Merchant Investments</a>
+                        <a class="text-white text-decoration-none" href="https://winterasiatours.com" target="_blank">Winter Asia Tours & Travels</a>
+                        <p class="mb-0">Degital Zip</p>
+                        <a class="text-white text-decoration-none" href="https://ammaiputhai.com" target="_blank">Ammai Puthai</a>
+                        <p class="mb-0">Hasarel</p>
+                        <p class="mb-0">Dilshan Fashion Outlet</p>
+                        <p class="mb-0">Union Timbers</p>
+                        <p class="mb-0">Union Hardware</p>
+                        <p class="mb-0">Herb & Carb</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card rounded-5 p-3">
+        <div class="card-body">
+            <div class="d-flex flex-column gap-3">
+                <div class="d-flex flex-column gap-3">
+                    <h2 class="mb-0">dubsndot Clients</h2>
+                    <p class="mb-0">I joined dubsndot, a web design agency based in Melbourne, Australia, as a freelance web developer and advisor. Although I did not work directly with their clients or developers, I had the opportunity to contribute my expertise and provide freelance services such as web development, maintenance, and system administration.</p>
+                    <div class="d-flex flex-column gap-3 gap-lg-0">
+                        <p class="mb-0">EIA Painting</p>
+                        <p class="mb-0">Eco Mart</p>
+                        <a class="text-white text-decoration-none" href="https://www.enlightensl.com" target="_blank">Enlighten</a>
+                        <p class="mb-0">AdWay</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card rounded-5 p-3">
+        <div class="card-body">
+            <div class="d-flex flex-column gap-3">
+                <div class="d-flex flex-column gap-3">
+                    <h2 class="mb-0">Phyxle Clients</h2>
+                    <p class="mb-0">I first joined Phyxle, a well-known web design agency based in Colombo, Sri Lanka, as a freelance web developer. After some time, I transitioned into a full-time role and eventually became the director of the company. In this position, I had the opportunity to collaborate with many clients and work alongside junior developers. As director and senior developer, my responsibilities spanned a wide range—from interviewing fresh recruits to designing software architectures, developing solutions, and managing their full lifecycle through services such as DevOps and system administration.</p>
+                    <div class="d-flex flex-column gap-3 gap-lg-0">
+                        <a class="text-white text-decoration-none" href="https://iconique.tech" target="_blank">Iconique</a>
+                        <a class="text-white text-decoration-none" href="https://www.jeewaeducation.lk" target="_blank">Jeewa Education</a>
+                        <a class="text-white text-decoration-none" href="https://cdi.lk" target="_blank">CDI Investments</a>
+                        <a class="text-white text-decoration-none" href="https://longjiangling.com" target="_blank">Long Jiang Ling</a>
+                        <a class="text-white text-decoration-none" href="https://rhtraining.lk" target="_blank">RH Training</a>
+                        <a class="text-white text-decoration-none" href="https://healthcareerme.com" target="_blank">Health Career Me</a>
+                        <p class="mb-0">Ethauleem</p>
+                        <a class="text-white text-decoration-none" href="https://kenaracapital.com" target="_blank">Kenara Capital</a>
+                        <p class="mb-0">Bon Arabica</p>
+                        <a class="text-white text-decoration-none" href="https://microcells.net" target="_blank">Microcells</a>
+                        <p class="mb-0">Heiger</p>
+                        <a class="text-white text-decoration-none" href="https://econsulate.net" target="_blank">Econsulate</a>
+                        <a class="text-white text-decoration-none" href="https://aerotek.lk" target="_blank">Aerotek</a>
+                        <a class="text-white text-decoration-none" href="https://tissl.lk" target="_blank">The International Schools Of Sri Lanka</a>
+                        <p class="mb-0">Synerg BPO</p>
+                        <a class="text-white text-decoration-none" href="https://fastauktion.ch" target="_blank">Fast Auktion</a>
+                        <a class="text-white text-decoration-none" href="https://thehansarasavi.lk" target="_blank">Thehan Sarasavi</a>
+                        <a class="text-white text-decoration-none" href="https://exelsolutions.com" target="_blank">Exel Solutions</a>
+                        <a class="text-white text-decoration-none" href="https://www.logipharma.lk" target="_blank">LogiPharma</a>
+                        <a class="text-white text-decoration-none" href="https://csbm.edu.lk" target="_blank">CSBM</a>
+                        <a class="text-white text-decoration-none" href="https://www.okischool.lk" target="_blank">OKI</a>
+                        <p class="mb-0">Visira Radio</p>
+                        <a class="text-white text-decoration-none" href="https://pineview.lk" target="_blank">Pine View</a>
+                        <a class="text-white text-decoration-none" href="https://slavo.lk" target="_blank">Sri Lanka Association Of Vessel Operators</a>
+                        <a class="text-white text-decoration-none" href="https://authenticitiessrilanka.com" target="_blank">Authenticities</a>
+                        <a class="text-white text-decoration-none" href="https://tropiculture.travel" target="_blank">Tropiculture</a>
+                        <a class="text-white text-decoration-none" href="https://www.myanysoft.com" target="_blank">My Any Soft</a>
+                        <a class="text-white text-decoration-none" href="https://www.lcbfinance.lk" target="_blank">LCB Finance</a>
+                        <p class="mb-0">Capital Clicks</p>
+                        <p class="mb-0">VI Associates</p>
+                        <p class="mb-0">Tec Panthiya</p>
+                        <p class="mb-0">GSM Sri Lanka</p>
+                        <p class="mb-0">Success World</p>
+                        <p class="mb-0">Sampath Bandara</p>
+                        <p class="mb-0">Malavi</p>
+                        <p class="mb-0">SL GSM Unlock Server</p>
+                        <p class="mb-0">Woow</p>
+                        <p class="mb-0">BN Vehicles</p>
+                        <p class="mb-0">GC Books</p>
+                        <p class="mb-0">NetCoins</p>
+                        <p class="mb-0">Wood.lk</p>
+                        <p class="mb-0">My Gems</p>
+                        <p class="mb-0">SA Unlock GSM Server</p>
+                        <p class="mb-0">Sola</p>
+                        <p class="mb-0">Sa Ga</p>
+                        <p class="mb-0">TicketZone</p>
+                        <p class="mb-0">Eventive</p>
+                        <p class="mb-0">Ready Me Jobs</p>
+                        <p class="mb-0">Call Yalu</p>
+                        <p class="mb-0">The Ivory Candles</p>
+                        <a class="text-white text-decoration-none" href="https://www.nsb.lk" target="_blank">NSB</a>
+                        <p class="mb-0">Serendib Leisure Tours</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card rounded-5 p-3">
+        <div class="card-body">
+            <div class="d-flex flex-column gap-3">
+                <div class="d-flex flex-column gap-3">
+                    <h2 class="mb-0">Novart Clients</h2>
+                    <p class="mb-0">Novart, a well-known advertising agency based in Ratmalana, Sri Lanka, was the first company where I began my career. At Novart, I gained extensive knowledge in web development and had the opportunity to collaborate with many clients while working alongside senior developers. My role involved providing both web development and maintenance services.</p>
+                    <div class="d-flex flex-column gap-3 gap-lg-0">
+                        <p class="mb-0">Beaudire</p>
+                        <p class="mb-0">Bunglow On 53</p>
+                        <a class="text-white text-decoration-none" href="https://www.estheticentre.com" target="_blank">Estheti Centre</a>
+                        <a class="text-white text-decoration-none" href="https://www.sothys.lk" target="_blank">Sothys</a>
+                        <a class="text-white text-decoration-none" href="https://tradeads.lk" target="_blank">TradeAds</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
