273e2c7bd156a5ba8bf489bdaa229f3c16b9c73d
Enindu Alahapperuma <enindu@gmail.com>
2025-09-29 12:05:57 +0530

    Update blog page

--- resources/views/app/blog/index.blade.php
+++ resources/views/app/blog/index.blade.php
@@ -17,16 +17,118 @@
     </div>
     <div class="card rounded-5 p-3">
         <div class="card-body">
-            <div class="d-flex flex-column gap-3 gap-lg-0">
-                <a class="text-white text-decoration-none" href="{{ route('blog.blog-9') }}">How Poor Development Skills Break Internet</a>
-                <a class="text-white text-decoration-none" href="{{ route('blog.blog-8') }}">Running Shell Commands Within Shared Hosting</a>
-                <a class="text-white text-decoration-none" href="{{ route('blog.blog-7') }}">Playing With DNS Clients</a>
-                <a class="text-white text-decoration-none" href="{{ route('blog.blog-6') }}">Understanding DNS</a>
-                <a class="text-white text-decoration-none" href="{{ route('blog.blog-5') }}">Shared Hosting Explained For Clients</a>
-                <a class="text-white text-decoration-none" href="{{ route('blog.blog-4') }}">Playing With HTTP Servers</a>
-                <a class="text-white text-decoration-none" href="{{ route('blog.blog-3') }}">Understanding HTTP</a>
-                <a class="text-white text-decoration-none" href="{{ route('blog.blog-2') }}">Ultimate Web Development Guide For Clients</a>
-                <a class="text-white text-decoration-none" href="{{ route('blog.blog-1') }}">I Have Revamped My Website</a>
+            <div class="d-flex flex-column gap-3">
+                <div class="d-flex flex-column gap-3">
+                    <h2 class="mb-0">How Poor Development Skills Break Internet</h2>
+                    <p class="mb-0">The most important part of a website's lifecycle is probably the development skills, with infrastructure skills coming second. I often come across websites b...</p>
+                    <div>
+                        <a class="btn btn-primary rounded-pill px-5 py-2" href="{{ route('blog.blog-9') }}">View Article</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card rounded-5 p-3">
+        <div class="card-body">
+            <div class="d-flex flex-column gap-3">
+                <div class="d-flex flex-column gap-3">
+                    <h2 class="mb-0">Running Shell Commands Within Shared Hosting</h2>
+                    <p class="mb-0">In a previous article, I discussed shared hosting. In this one, I'll be experimenting with a shared hosting server. My goal is to highlight a vulnerability a...</p>
+                    <div>
+                        <a class="btn btn-primary rounded-pill px-5 py-2" href="{{ route('blog.blog-8') }}">View Article</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card rounded-5 p-3">
+        <div class="card-body">
+            <div class="d-flex flex-column gap-3">
+                <div class="d-flex flex-column gap-3">
+                    <h2 class="mb-0">Playing With DNS Clients</h2>
+                    <p class="mb-0">In a previous article, I provided a low-level overview of DNS servers. While it wasn't an exhaustive guide, it covered the fundamentals to help you understan...</p>
+                    <div>
+                        <a class="btn btn-primary rounded-pill px-5 py-2" href="{{ route('blog.blog-7') }}">View Article</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card rounded-5 p-3">
+        <div class="card-body">
+            <div class="d-flex flex-column gap-3">
+                <div class="d-flex flex-column gap-3">
+                    <h2 class="mb-0">Understanding DNS</h2>
+                    <p class="mb-0">Most developers have a high-level understanding of DNS. However, for web developers, grasping DNS at a low level is crucial. Writing yet another article expl...</p>
+                    <div>
+                        <a class="btn btn-primary rounded-pill px-5 py-2" href="{{ route('blog.blog-6') }}">View Article</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card rounded-5 p-3">
+        <div class="card-body">
+            <div class="d-flex flex-column gap-3">
+                <div class="d-flex flex-column gap-3">
+                    <h2 class="mb-0">Shared Hosting Explained For Clients</h2>
+                    <p class="mb-0">This isn't just another article explaining shared hosting. Instead, I'll break it down from a system administrator's perspective—why you should never use sha...</p>
+                    <div>
+                        <a class="btn btn-primary rounded-pill px-5 py-2" href="{{ route('blog.blog-5') }}">View Article</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card rounded-5 p-3">
+        <div class="card-body">
+            <div class="d-flex flex-column gap-3">
+                <div class="d-flex flex-column gap-3">
+                    <h2 class="mb-0">Playing With HTTP Servers</h2>
+                    <p class="mb-0">In a previous article, I provided a high-level overview of HTTP. In this article, I will dive into the low-level implementation of HTTP, covering topics such...</p>
+                    <div>
+                        <a class="btn btn-primary rounded-pill px-5 py-2" href="{{ route('blog.blog-4') }}">View Article</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card rounded-5 p-3">
+        <div class="card-body">
+            <div class="d-flex flex-column gap-3">
+                <div class="d-flex flex-column gap-3">
+                    <h2 class="mb-0">Understanding HTTP</h2>
+                    <p class="mb-0">There are countless articles and videos on the internet explaining what HTTP is. If I were to write this post in the same way, it would just be another one a...</p>
+                    <div>
+                        <a class="btn btn-primary rounded-pill px-5 py-2" href="{{ route('blog.blog-3') }}">View Article</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card rounded-5 p-3">
+        <div class="card-body">
+            <div class="d-flex flex-column gap-3">
+                <div class="d-flex flex-column gap-3">
+                    <h2 class="mb-0">Ultimate Web Development Guide For Clients</h2>
+                    <p class="mb-0">I come across numerous web development groups on Facebook every day. These groups are filled with people—some searching for web developers at low prices, whi...</p>
+                    <div>
+                        <a class="btn btn-primary rounded-pill px-5 py-2" href="{{ route('blog.blog-2') }}">View Article</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card rounded-5 p-3">
+        <div class="card-body">
+            <div class="d-flex flex-column gap-3">
+                <div class="d-flex flex-column gap-3">
+                    <h2 class="mb-0">I Have Revamped My Website</h2>
+                    <p class="mb-0">I have upgraded the Laravel framework to version 12, made significant improvements to the front-end design, and added service pages to the website. This mean...</p>
+                    <div>
+                        <a class="btn btn-primary rounded-pill px-5 py-2" href="{{ route('blog.blog-1') }}">View Article</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
